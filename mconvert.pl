60879a811328000af181bf1d9a601cd87d92e954
Tae-Gil Noh <noh@cl.uni-heidelberg.de>
2012-12-26 10:28:33 +0100

    - completed mconvert script. small test output added in /data

--- mconvert.pl
+++ mconvert.pl
@@ -6,10 +6,63 @@
 use warnings; 
 use strict; 
 
-# grap one <sentence> - </sentence> 
-# extract sentence part <s> - </s> 
-# run convert_sentence 
+sub extract_s(@);
+sub convert_sentence(@); 
 
+# grap one <sentence> - </sentence> 
+my $catch=0; 
+my @sentence; 
+while(<STDIN>)
+{
+    $catch = 1 if (/^<sentence>/); 	# start capture
+    if (/^<\/sentence>/)
+    {
+	# we've captured one full sentence. call <s> extractor
+	my @s = extract_s(@sentence); 
+	convert_sentence(@s); # this sub converts and prints the <s> content	
+	print "\n"; # sentence separater... 
+
+	# resetting variables for next iteration ... 
+	@sentence=(); 
+	$catch=0; 
+    }
+    if ($catch)
+    {
+	push @sentence, $_; 
+    }
+}
+
+
+# extract sentence part <s> - </s>, from @array that holds <sentence> content. 
+sub extract_s(@)
+{
+    my @input = @_; 
+
+    # trim front until <s>
+    while(1)
+    {
+	my $l = shift @input; 
+	last if ($l =~ /^<s>/); 
+    }
+    
+    #sanity check 
+    warn "extract_s: ill formed input @input" unless (scalar (@input)); 
+    
+    # trim end till meeting </s> 
+    while(scalar(@input))
+    {
+	my $l = pop @input; 
+	last if ($l =~ /^<\/s>/)
+    }
+
+    #sanity check 
+    warn "extract_s: ill formed input @input" unless (scalar (@input)); 
+
+    return @input; 
+}
+
+
+# converts and prints single <s> content to STDOUT 
 sub convert_sentence(@)
 {
     my @lines = @_; # the lines only holds actual data, not XML tags. 
@@ -18,11 +71,11 @@ sub convert_sentence(@)
     foreach (@lines)
     {
 	my @item = split /\s+/; 
-	warn "Wrong input? : $_\n" unless (scalar (@item) != 3); 
+	warn "Wrong input? : $_\n" unless (scalar (@item) == 3); 
 	my $form = $item[0]; 
 	my $pos = $item[1]; 
 	my $lemma = $item[2]; 
-	my $cpos = ucfirst $pos; 
+	my $cpos = substr $pos, 0, 1; 
 	
 	# ID, FORM, LEMMA, CPOSTAG, POSTAG, FEATS, HEAD, DEPREL
 	print $token_id, "\t"; 
