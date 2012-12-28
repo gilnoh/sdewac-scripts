449feb8cd1659e4e7432c11d30ed28b31f673036
Tae-Gil Noh <noh@cl.uni-heidelberg.de>
2012-12-28 17:34:49 +0100

    - tested and made sure mrecover.pl properly works.

--- mrecover.pl
+++ mrecover.pl
@@ -3,7 +3,7 @@
 use strict;
 use warnings; 
 
-# recovers back missing POS from MST parser output (CONLL setup) 
+# recovers back missing POS (and lemma) from MST parser output (CONLL setup) 
 
 die "needs two files (conll input, and mstparsed conll output)" unless ($ARGV[1]); 
 die "unable to read files" unless ((-r $ARGV[0]) and (-r $ARGV[1])); 
@@ -26,13 +26,18 @@ while(<FILEPARSED>)
 	my @rawtab = split /\t/, $raw_line;
 	my @parsedtab = split /\t/, $parsed_line; 
 
-	die "sanity check fails! Two files are not of same input? $raw_line $parsed_line" unless (($rawtab[0] eq $parsedtab[0]) and ($rawtab[1] eq $parsedtab[1]) and ($rawtab[2] eq $parsedtab[2]) and ($rawtab[3] and $parsedtab[3])); 
+	# two sanity checks 
+	#die "sanity check fails! Two files are not of same input? $raw_line $parsed_line" unless (($rawtab[0] eq $parsedtab[0]) and ($rawtab[1] eq $parsedtab[1]) and ($rawtab[3] and $parsedtab[3])); 
+	die "sanity check fails! Two files are not of same input? $raw_line $parsed_line" unless (($rawtab[0] eq $parsedtab[0])); 	
+	die "eh, arg1 should be raw file, arg2 should be parsed file" unless ((length $rawtab[4]) > (length $parsedtab[4])); 
 
 	# replace the 5th tab from $raw_line one 
-	$parsedtab[4] = $rawtab[4]; 
+	$parsedtab[4] = $rawtab[4]; # POS 
+	$parsedtab[2] = $rawtab[2]; # lemma 
+	$parsedtab[1] = $rawtab[1]; # token string (e.g. <num> back to 129, etc)
 
 	# compose new output_line 
-	$output_line = join('\t', @parsedtab); 
+	$output_line = join("\t", @parsedtab); 
     }
     print $output_line; 
 }
